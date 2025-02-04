9c7ce56a200c4bcda580b61ca3c82be5daea38a5
ss-gayu <121176958+ss-gayu@users.noreply.github.com>
2025-02-04 13:33:27 +0530

    Add files via upload

---
+++ WINTER PEP/day16/teamB.js
@@ -0,0 +1,30 @@
+function sum(a,b){
+    //parameters
+    console.log("---calculate sum---");
+        return a+b;
+    
+}
+
+
+function multiply(a,b){
+
+
+console.log("calculating multiply");
+}
+const container={
+    sum:sum,
+    mul:multiply,
+
+};
+module.exports=container;
+
+
+
+//funtion declaration
+
+    function sum(a,b){
+        console.log(a-b);
+    }
+function sum(a,b){
+
+}
